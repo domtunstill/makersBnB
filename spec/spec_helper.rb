69547089aeb9f426284fb23089866ae0b0d2aaea
Olly Holovchenko <oleh.holo@gmail.com>
2019-10-21 18:24:37 +0100

    Update spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+ENV['RACK_ENV'] = 'test' 
+
 require 'simplecov'
 require 'simplecov-console'
 
@@ -15,9 +17,8 @@ require 'sinatra/activerecord'
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require_relative './setup_test_database'
 
-ENV['RACK_ENV'] = 'test' 
-ENV['ENVIRONMENT'] = 'test'
-ENV['RAILS_ENV'] = 'test'
+# ENV['ENVIRONMENT'] = 'test'
+# ENV['RAILS_ENV'] = 'test'
 
 Capybara.app = MakersBnb
 
