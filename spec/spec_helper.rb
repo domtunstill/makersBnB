fe9a85caab13e652c83cadb26e089dd76d6cbfb8
Olly Holovchenko <oleh.holo@gmail.com>
2019-10-21 18:44:26 +0100

    Update spec_helper. Add gitignore

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,7 @@
-
-require 'simplecov'
-require 'simplecov-console'
-=======
 ENV['RACK_ENV'] = 'test' 
 
 require 'simplecov'
 require 'simplecov-console'
-
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
                                                                  SimpleCov::Formatter::Console,
@@ -22,13 +17,8 @@ require 'sinatra/activerecord'
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require_relative './setup_test_database'
 
-ENV['RACK_ENV'] = 'test'
-ENV['ENVIRONMENT'] = 'test'
-Capybara.app = MakersBnb
-=======
-
+# ENV['RACK_ENV'] = 'test'
 # ENV['ENVIRONMENT'] = 'test'
-# ENV['RAILS_ENV'] = 'test'
 
 Capybara.app = MakersBnb
 
