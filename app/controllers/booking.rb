e82f93eec40e24b1741af5a48ee95242411e0946
Duncan Skinner <duncanskinner94@gmail.com>
2019-10-22 13:52:37 +0100

    confirms new booking

--- app/controllers/booking.rb
+++ app/controllers/booking.rb
@@ -4,7 +4,12 @@ class MakersBnb < Sinatra::Base
   get '/property/:id/booking/new' do
     @property_id = params[:id]
     @property = Property.find(params[:id])
+    flash[:notice] = "Booking confirmed"
 
     erb :'booking/new'
+  end
+
+  post '/property/:id/booking' do
+    redirect "/user/#{current_user.id}"
   end
 end
--- spec/features/user_requests_property_spec.rb
+++ spec/features/user_requests_property_spec.rb
@@ -4,9 +4,27 @@ feature 'bookings' do
     create_user2
     property = Property.create(user_id: user.id, name: 'Makers', description: '52 Commercial Place', price_per_night: 133)
 
-    visit "/property/#{property.id}/bookings/new"
+    visit "/property/#{property.id}/booking/new"
     expect(page).to have_content("Makers", "52 Commercial Place")
+    expect(page).to have_content("Select check in date")
+    expect(page).to have_content("Select check out date")
     expect(page).to have_button("request_booking")
+  end
 
+  scenario 'booking is confirmed' do
+    user = create_user1
+    property = Property.create(user_id: user.id, name: 'Makers', description: '52 Commercial Place', price_per_night: 133)
+
+    visit '/login'
+    fill_in 'email', with: 'user@user.com'
+    fill_in 'password', with: 'password'
+    click_button 'Submit'
+
+    visit "/property/#{property.id}/booking/new"
+    fill_in 'check_in', with: '22/10/2019'
+    fill_in 'check_out', with: '23/10/2019'
+    click_button 'request_booking'
+
+    expect(page).to have_button('Create')
   end
 end
